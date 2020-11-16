422993b394ab36b722ef5bc3f9f5aa1674a1230e
ma <ma@sep.de>
2020-11-16 17:02:42 +0100

    rework extent handling

--- libvirtnbdbackup/extenthandler/extenthandler.py
+++ libvirtnbdbackup/extenthandler/extenthandler.py
@@ -1,9 +1,16 @@
+import logging
+
 class Extent(object):
     def __init__(self):
         self.data = False
         self.zero = False
         self.length = 0
         self.offset = 0
+
+class _ExtentObj(object):
+    def __init__(self):
+        self.length = None
+        self.type = None
 
 class ExtentHandler(object):
 
@@ -19,6 +26,7 @@ class ExtentHandler(object):
     """
 
     def __init__(self, nbdFh, metaContext):
+        self._log = logging.getLogger(__name__)
         self. useQemu = False
         if nbdFh.__class__.__name__ == "qemuHelper":
             self.useQemu = True
@@ -29,13 +37,19 @@ class ExtentHandler(object):
             self._metaContext = "base:allocation"
         else:
             self._metaContext = metaContext
+
+        self._log.debug("Meta context: %s" % self._metaContext)
         self._maxRequestBlock = 4294967295
         self._align = 512
 
     def _getExtentCallback(self, metacontext, offset, entries, status):
+        self._log.debug("Metacontext is: %s" % metacontext)
         if metacontext != self._metaContext:
+            self._log.error("Meta context does not match")
             return
-        self._extentEntries.append(entries)
+        for entry in entries:
+            self._extentEntries.append(entry)
+        self._log.debug("entries: %s" % len(self._extentEntries))
 
     def _setRequestAligment(self):
         align = self._nbdFh.get_block_size(0)
@@ -63,102 +77,81 @@ class ExtentHandler(object):
 
         return extents
 
+    def _extentsToObj(self):
+        extentSizes = self._extentEntries[0::2]
+        extentTypes = self._extentEntries[1::2]
+        assert len(extentSizes) == len(extentTypes)
+        ct = 0
+        extentList = []
+        while ct < len(extentSizes):
+            extentObj = _ExtentObj()
+            extentObj.length = extentSizes[ct]
+            extentObj.type = extentTypes[ct]
+            extentList.append(extentObj)
+            ct+=1
+
+        return extentList
+
+    def _unifyExtents(self, extentObjects):
+        cur = None
+        for myExtent in extentObjects:
+            if cur == None:
+                cur = myExtent
+            elif cur.type == myExtent.type:
+                cur.length += myExtent.length
+            else:
+                yield cur
+                cur = myExtent
+
     def queryExtentsNbd(self):
         maxRequestLen = self._setRequestAligment()
         offset = 0
         size = self._nbdFh.get_size()
+        self._log.debug("Size returned from NDB server: %s" % size)
+        lastExtentLen = len(self._extentEntries)
         while offset < size:
             if size < maxRequestLen:
                 request_length=size
             else:
                 request_length = min(size - offset, maxRequestLen)
+            self._log.debug('Block status request length: %s' % request_length)
             self._nbdFh.block_status(request_length, offset, self._getExtentCallback)
-            offset+=request_length
+            if len(self._extentEntries) == 0:
+                self._log.error("No extents found")
+                return False
 
-        ct = 0
-        while ct<len(self._extentEntries):
-            last = ct-1
-            if last < 0 or ct == 0:
-                pass
-            else:
-                last_size = self._extentEntries[last][-2]
-                last_type = self._extentEntries[last][-1]
-                curr_size = self._extentEntries[ct][0]
-                curr_type = self._extentEntries[ct][1]
+            offset+=sum(self._extentEntries[lastExtentLen::2])
+            lastExtentLen = len(self._extentEntries)
 
-                # TODO: what if the types of the extents
-                # do not match?
-                if curr_type != last_type:
-                    pass
+        self._log.debug('Extents: %s' % self._extentEntries)
+        self._log.debug('Number Extents: %s' % len(self._extentEntries))
 
-                # now remove the last entry of the previously
-                # processed block
-                self._extentEntries[last].pop()
-                self._extentEntries[last].pop()
-
-                # and set the first entry of the current block
-                # to the coalesced size
-                self._extentEntries[ct][0] = last_size+curr_size
-            ct+=1
-
-        return self._extentEntries
+        return self._extentsToObj()
 
     def queryBlockStatus(self, extentList=None):
-        if extentList == None and self.useQemu == False:
-            self.queryExtentsNbd()
-        else:
-            return self.queryExtentsQemu()
-
-        if len(self._extentEntries) < 1:
-            return None
-
         extentList = []
-        startCt = 0
-        for entry in self._extentEntries:
-            extentType = entry[1::2]
-            extentLength = entry[0::2]
-
-            ct=0
-            if startCt == 0:
-                start=0
+        start = 0
+        for extent in self._unifyExtents(self.queryExtentsNbd()):
+            extObj = Extent()
+            if self._metaContext == "base:allocation":
+                assert extent.type in (0,2,3)
+                if extent.type == 0:
+                    extObj.data = True
+                elif extent.type == 2:
+                    extObj.data = True
+                elif extent.type == 3:
+                    extObj.data = False
             else:
-                start=startCt
-
-            for ct in range(0,int(len(entry)/2)):
-                extObj = Extent()
-                """
-                    The extent types are as follows:
-
-                    For full backup:
-                        case 0  ("allocated")
-                        case 1: ("hole")
-                        case 2: ("zero")
-                        case 3: ("hole,zero")
-                    For checkpoint based inc/diff:
-                        case 0: ("clean")
-                        case 1: ("dirty")
-                """
-                if self._metaContext == "base:allocation":
-                    assert extentType[ct] in (0,2,3)
-                    if extentType[ct] == 0:
-                        extObj.data = True
-                    elif extentType[ct] == 2:
-                        extObj.data = True
-                    elif extentType[ct] == 3:
-                        extObj.data = False
+                assert extent.type in (0,1)
+                if extent.type == 1:
+                    extObj.data = True
                 else:
-                    assert extentType[ct] in (0,1)
-                    if extentType[ct] == 1:
-                        extObj.data = True
-                    else:
-                        extObj.data = False
+                    extObj.data = False
 
-                extObj.offset = start
-                extObj.length = extentLength[ct]
+            extObj.offset = start
+            extObj.length = extent.length
+            extentList.append(extObj)
+            start+=extent.length
 
-                extentList.append(extObj)
-                start+=extentLength[ct]
-                ct+=1
-            startCt=start
-
+        self._log.debug('Returning extent list with %s objects' % len(extentList))
         return extentList
