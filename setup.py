b7ffdee6f99b13be0ac118b37933f1502896bf78
ma <ma@sep.de>
2021-03-29 08:36:12 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import os
 from setuptools import setup, find_packages
-__version__ = "0.1"
+__version__ = "0.6"
 
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-# the setup
 setup(
     name='virtndbbackup',
     version=__version__,
