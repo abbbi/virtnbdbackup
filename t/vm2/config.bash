e25b80316feea130c6192a22546c8b6b774fe4de
Michael Ablassmeier <abi@grinser.de>
2021-05-01 20:48:24 +0200

    fix url to image

--- t/vm2/config.bash
+++ t/vm2/config.bash
@@ -9,7 +9,7 @@ VM="vm2"
 # are applied to the image
 # VM image is qcow2 so no persistent bitmaps
 # are supported, create only copy backups
-VM_IMAGE_URL="https://cdimage.debian.org/cdimage/openstack/archive/10.6.0/debian-10.6.0-openstack-amd64.qcow2"
+VM_IMAGE_URL="https://chuangtzu.ftp.acc.umu.se/cdimage/openstack/archive/10.6.0/debian-10.6.0-openstack-amd64.qcow2"
 VM_IMAGE="debian-10.6.0-openstack-amd64.qcow2"
 
 EXTENT_OUTPUT1="Got 866 extents"
