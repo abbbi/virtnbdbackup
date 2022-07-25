0617517731c85ae5e2ed4a4934e71ed8e8e7c6ec
Michael Ablassmeier <abi@grinser.de>
2022-07-25 17:50:27 +0200

    version 0.82

---
+++ libvirtnbdbackup/argopt.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+    Copyright (C) 2022  Michael Ablassmeier <abi@grinser.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from getpass import getuser
+from libvirtnbdbackup import __version__
+
+
+def addRemoteArgs(opt):
+    """Common remote backup arguments"""
+    opt.add_argument(
+        "-U",
+        "--uri",
+        default="qemu:///system",
+        required=False,
+        type=str,
+        help="Libvirt connection URI. (default: %(default)s)",
+    )
+    opt.add_argument(
+        "--user",
+        default=None,
+        required=False,
+        type=str,
+        help="User to authenticate against libvirtd. (default: %(default)s)",
+    )
+    opt.add_argument(
+        "--ssh-user",
+        default=getuser() or None,
+        required=False,
+        type=str,
+        help=(
+            "User to authenticate against remote sshd: "
+            "used for remote copy of files. (default: %(default)s)"
+        ),
+    )
+    opt.add_argument(
+        "--password",
+        default=None,
+        required=False,
+        type=str,
+        help="Password to authenticate against libvirtd. (default: %(default)s)",
+    )
+    opt.add_argument(
+        "-P",
+        "--nbd-port",
+        type=int,
+        default=10809,
+        required=False,
+        help=(
+            "Remote NBD port to start backup on, should be unique for each"
+            " started backup. (default: %(default)s)"
+        ),
+    )
+
+
+def addDebugArgs(opt):
+    """Common debug arguments"""
+    opt.add_argument(
+        "-v",
+        "--verbose",
+        default=False,
+        help="Enable debug output",
+        action="store_true",
+    )
+    opt.add_argument(
+        "-V",
+        "--version",
+        default=False,
+        help="Show version and exit",
+        action="version",
+        version=__version__,
+    )
