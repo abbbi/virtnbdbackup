0e2031c86f2906217680cd870ae84c4f091cb92e
Michael Ablassmeier <abi@grinser.de>
2022-11-11 13:27:53 +0100

    rebase tests on github on ubuntu 22.04, rework sequence restore

--- t/vm3/config.bash
+++ t/vm3/config.bash
@@ -7,9 +7,18 @@ VM_IMAGE="${VM}/vm3-*.qcow2"
 
 
 # following outputs are expected for this vm image
-EXTENT_OUTPUT1="Got 7 extents to backup."
-EXTENT_OUTPUT2="1048576 bytes disk size"
-EXTENT_OUTPUT3="327680 bytes of data extents to backup"
+if [ -z $GITHUB_JOB ]; then
+    EXTENT_OUTPUT1="Got 7 extents to backup."
+    EXTENT_OUTPUT2="1048576 bytes disk size"
+    EXTENT_OUTPUT3="327680 bytes of data extents to backup"
 
-DATA_SIZE="327680"
+    DATA_SIZE="327680"
+else
+    EXTENT_OUTPUT1="Got 5 extents to backup."
+    EXTENT_OUTPUT2="1048576 bytes disk size"
+    EXTENT_OUTPUT3="131072 bytes of data extents to backup"
+
+    DATA_SIZE="131072"
+fi
+
 VIRTUAL_SIZE="1048576"
