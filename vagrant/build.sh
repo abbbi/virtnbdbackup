2708940b32d5f150cd8b4e47ca5a9e9b8b71f792
Michael Ablassmeier <abi@grinser.de>
2023-08-22 22:43:43 +0200

    Version 1.9.38

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -3,5 +3,8 @@ set -e
 export VAGRANT_CWD=$(pwd)/debian/
 vagrant up && vagrant destroy -f
 
-export VAGRANT_CWD=$(pwd)/rhel/
+export VAGRANT_CWD=$(pwd)/rhel8/
+vagrant up && vagrant destroy -f
+
+export VAGRANT_CWD=$(pwd)/rhel9/
 vagrant up && vagrant destroy -f
