769883a698d63562951149c8ab97acec5ab609af
Michael Ablassmeier <abi@grinser.de>
2024-06-20 20:39:35 +0200

    fix some shellcheck warnings

--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -5,7 +5,7 @@ set -e
 SYSTEM_PKIDIR=/etc/pki/qemu/
 USER_PKIDIR=$HOME/.pki/libnbd/
 
-mkdir -p ${SYSTEM_PKIDIR} ${USER_PKIDIR}
+mkdir -p "${SYSTEM_PKIDIR}" "${USER_PKIDIR}"
 cat <<EOF >  certificate_authority_template.info
 cn = virtnbdbackup
 ca
@@ -38,7 +38,6 @@ certtool --generate-certificate \
 cp host1_server_key.pem ${SYSTEM_PKIDIR}/server-key.pem
 cp host1_server_certificate.pem ${SYSTEM_PKIDIR}/server-cert.pem
 
-
 cat <<EOF >  host1_client_template.info
 country = Country
 state = State
@@ -57,10 +56,9 @@ certtool --generate-certificate \
             --load-ca-privkey ca_key.pem \
             --outfile host1_client_certificate.pem
 
-cp host1_client_certificate.pem ${USER_PKIDIR}/client-cert.pem
-cp host1_client_key.pem ${USER_PKIDIR}/client-key.pem
-cp ca-cert.pem ${USER_PKIDIR}/ca-cert.pem
-
+cp host1_client_certificate.pem "${USER_PKIDIR}"/client-cert.pem
+cp host1_client_key.pem "${USER_PKIDIR}"/client-key.pem
+cp ca-cert.pem "${USER_PKIDIR}"/ca-cert.pem
 
 sed -i 's/#backup_tls_x509_verify.*/backup_tls_x509_verify=0/' /etc/libvirt/qemu.conf
 systemctl restart libvirtd
