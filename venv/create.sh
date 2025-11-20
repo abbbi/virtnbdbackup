06ab0f18b57da3e27c631411d71284f5527111cb
ma <ma@sep.de>
2025-11-20 09:40:39 +0100

    venv/create.sh: fix shellcheck warnings

--- venv/create.sh
+++ venv/create.sh
@@ -8,27 +8,28 @@ LIBNBD_VERSION="libnbd-1.22.5"
 set -e
 
 BRANCH="master"
-if [ ! -z $1 ]; then
+if [ -n "$1" ]; then
     BRANCH="$1"
 fi
 rm -rf virtnbdbackup
-rm -rf ${VENVDIR}
-rm -rf ${LIBNBD_VERSION}
+rm -rf "${VENVDIR}"
+rm -rf "${LIBNBD_VERSION}"
 rm -rf libnbd-*
 
-virtualenv -p $(which python3) ${VENVDIR}
-git clone https://github.com/abbbi/virtnbdbackup.git -b ${BRANCH}
-source ${VENVDIR}/bin/activate
+virtualenv -p "$(which python3)" "${VENVDIR}"
+git clone https://github.com/abbbi/virtnbdbackup.git -b "${BRANCH}"
+# shellcheck source=/dev/null
+source "${VENVDIR}/bin/activate"
 pip install -U setuptools
 curl https://download.libguestfs.org/libnbd/${LIBNBD_MAJ}/${LIBNBD_VERSION}.tar.gz  > ${LIBNBD_VERSION}.tar.gz
 tar xzf ${LIBNBD_VERSION}.tar.gz
 cd ${LIBNBD_VERSION}
 export bashcompdir=/tmp
-./configure --prefix=${VENVDIR} --exec-prefix=${VENVDIR} \
+./configure --prefix="${VENVDIR}" --exec-prefix="${VENVDIR}" \
     --disable-ocaml \
     --disable-golang \
     --disable-fuse
-make -j $(nproc)
+make -j "$(nproc)"
 make install
 cd ..
 python -m pip install -r virtnbdbackup/requirements.txt 
